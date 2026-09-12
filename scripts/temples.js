b7f0516200cffafb3ca02653bee0c4070f7ae8bd
Sam Rowley <darthvremis@gmail.com>
2026-09-12 18:05:48 -0500

    Updated temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,14 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+const menuButton = document.getElementById("menu");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", function () {
+    nav.classList.toggle("show");
+    if (nav.classList.contains("show")) {
+        menuButton.textContent = "✕";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
