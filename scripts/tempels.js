c17eecb64c24516c3ed485c41415e3cca0ab4074
Sam Rowley <darthvremis@gmail.com>
2026-09-12 14:09:46 -0500

    Fixed page issues.

--- scripts/tempels.js
+++ scripts/tempels.js
@@ -1,12 +1,4 @@
-const today = new Date();
-const year = today.getFullYear();
+const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
-const lastModified = new Date(document.lastModified);
-const lastModifiedString = lastModified.toLocaleString();
-document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedString}`;
-
-const lastModifiedElement = document.getElementById("lastModified");
-if (lastModifiedElement) {
-  lastModifiedElement.textContent = `Last modified: ${lastModifiedString}`;
-}
+document.getElementById("lastModified").textContent = document.lastModified;
